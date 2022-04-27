da2fad35d9dd4c3be037ade0d7f3fe5357add53e
Overlord5465 <overlord5465@mail.ru>
2022-04-27 10:45:33 +0500

    bug theme

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,31 @@
-// -------------------theme----------------------
-let theme = document.querySelectorAll('link')[1];
+// // -------------------theme----------------------
+// let theme = document.querySelectorAll('link')[1];
 
-if(!localStorage.getItem('theme')) {
-  localStorage.setItem('theme', 'light')
-}
+// if(!localStorage.getItem('theme')) {
+//   localStorage.setItem('theme', 'light')
+// }
 
-defaultTheme()
+// defaultTheme()
 
-function defaultTheme(){
-  if(localStorage.getItem('theme') === 'light'){
-    theme.setAttribute('href', 'css/light-theme.css')
-  }else{
-    theme.setAttribute('href', 'css/dark-theme.css')
-  }
-}
+// function defaultTheme(){
+//   if(localStorage.getItem('theme') === 'light'){
+//     theme.setAttribute('href', 'css/light-theme.css')
+//   }else{
+//     theme.setAttribute('href', 'css/dark-theme.css')
+//   }
+// }
 
-let myButton2 = document.querySelectorAll('button')[1];
-myButton2.onclick = function() {
-  changeTheme();
-}
+// let myButton2 = document.querySelectorAll('button')[1];
+// myButton2.onclick = function() {
+//   changeTheme();
+// }
 
-function changeTheme(){
-  if(localStorage.getItem('theme') === 'light'){
-    theme.setAttribute('href', 'css/dark-theme.css')
-    localStorage.setItem('theme', 'dark')
-  }else{
-    theme.setAttribute('href', 'css/light-theme.css')
-    localStorage.setItem('theme', 'light')
-  }
-}
+// function changeTheme(){
+//   if(localStorage.getItem('theme') === 'light'){
+//     theme.setAttribute('href', 'css/dark-theme.css')
+//     localStorage.setItem('theme', 'dark')
+//   }else{
+//     theme.setAttribute('href', 'css/light-theme.css')
+//     localStorage.setItem('theme', 'light')
+//   }
+// }
