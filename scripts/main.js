e116f5b17db11ce578b923d9541538ded0e34c71
Overlord5465 <overlord5465@mail.ru>
2022-04-27 14:51:24 +0500

    adaptive layout experiments

--- scripts/main.js
+++ scripts/main.js
@@ -29,3 +29,16 @@
 //     localStorage.setItem('theme', 'light')
 //   }
 // }
+
+let userScreen = screen.width;
+
+if (userScreen < 500) {
+  let leftSide = document.querySelector("#left-side");
+  leftSide.parentNode.removeChild(leftSide);
+  let rightSide = document.querySelector("#right-side");
+  rightSide.parentNode.removeChild(rightSide);
+  let root = document.querySelector('#root');
+  root.classList.remove('three-columns');
+  let main = document.querySelector('main');
+  main.id = 'main-rounded';
+}
