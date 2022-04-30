88259a9d2fff5f864ce91c56245e8de8b1949255
Overlord5465 <overlord5465@mail.ru>
2022-04-30 14:29:35 +0500

    fixed canvas

--- scripts/cube.js
+++ scripts/cube.js
@@ -1,11 +1,11 @@
 const scene = new THREE.Scene();
 const canvas5465 = document.querySelector('#WebGL-start')
 
-const camera = new THREE.PerspectiveCamera(75, (main.offsetWidth * 0.95) / (window.innerHeight / 2), 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, (main.offsetWidth * 0.9) / (window.innerHeight / 2), 0.1, 1000);
 camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize((main.offsetWidth * 0.8), (window.innerHeight / 2));
+renderer.setSize((main.offsetWidth * 0.9), (window.innerHeight / 2));
 canvas5465.appendChild(renderer.domElement);
 
 let cube;
